097e510d2100c66f1b3f9356e3a7b26faee7fb84
unknown <lsplaver@yahoo.com>
2018-03-02 20:44:02 -0500

    Update - object and arayy of objects now working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,42 +56,69 @@ $(document).ready(function() {
     // console.log("The value of questionArray: " + questionArray);
     // // var tempArray = [];
     // let tempArray = questionArray;
-    function initQuestionObject() {
-        var questionObject = {
-            questionNum: 1,
-            questionTriviaQuestion: "A question",
-            questionAnswer1: "answer1",
-            questionAnswer2: "anser2",
-            questionAnswer3: "answertr3",
-            questionAnswer4: "ansdwert4",
-            questionCorrectAnswer: 0
-        };
+    var questionObject = {};
+    function initQuestionObject(questionObject) {
+//        /*var*/ questionObject = {
+//            questionNum: 1,
+//            questionTriviaQuestion: "A question",
+//            questionAnswer1: "answer1",
+//            questionAnswer2: "anser2",
+//            questionAnswer3: "answertr3",
+//            questionAnswer4: "ansdwert4",
+//            questionCorrectAnswer: 0
+//        };
+
+            questionObject.questionNum = 1,
+            questionObject.questionTriviaQuestion = "A question",
+            questionObject.questionAnswer1 = "answer1",
+            questionObject.questionAnswer2 = "anser2",
+            questionObject.questionAnswer3 = "answertr3",
+            questionObject.questionAnswer4 = "ansdwert4",
+            questionObject.questionCorrectAnswer = 0;
 
         return questionObject;
     }
 
+    console.log("The value of questionObject: " + questionObject.toString());
     var questionArray = [];
+    console.log("The value of questionArray is: " + questionArray);
+//    function questionMatrix(questionObject, questionArray) {
+//        // questionArray[] = aQuestArray;
+//        var anArray = true;
+//        console.log("The value of anArray before being a boolean: " + anArray);
+//        anArray = questionArray.isArray();
+//        console.log("The value of anArray after checking if questionArray is an array: " + anArray);
+//        questionArray.push(questionObject);
+//        for (var x = 0; x < questionArray.length; x++) {
+//            console.log("the value of questionArray[x] and questionObject[x] are: " + questionArray[x]);
+//        }
+//
+//        return questionArray;
+//
+//    }
+//
+//    initQuestionArray();
+    
+    initQuestionObject(questionObject);
 
-    function questionMatrix(questionObject, questionArray) {
-        // questionArray[] = aQuestArray;
-        var anArray;
-        console.log("The value of anArray before being a boolean: " + anArray);
-        anArray = questionArray.isArray();
-        console.log("The value of anArray after checking if questionArray is an array: " + anArray);
+//    questionMatrix(questionObject, questionArray[]);
         questionArray.push(questionObject);
         for (var x = 0; x < questionArray.length; x++) {
-            console.log("the value of questionArray[x] and questionObject[x] are: " + questionArray[x]);
+            console.log("the value of questionArray[x].questionNum is: "
+                + questionArray[x].questionNum);
+            console.log("the value of questionArray[x].questionTriviaQuestion\n\
+                is: " + questionArray[x].questionTriviaQuestion);
+            console.log("the value of questionArray[x].questionAnswer1 is: "
+                + questionArray[x].questionAnswer1);
+            console.log("the value of questionArray[x].questionAnswer2 is: "
+                + questionArray[x].questionAnswer2);
+            console.log("the value of questionArray[x].questionAnswer3 is: "
+                + questionArray[x].questionAnswer3);
+            console.log("the value of questionArray[x].questionAnswer4 is: "
+                + questionArray[x].questionAnswer4);
+            console.log("the value of questionArray[x].questionCorrectAnswer \n\
+                is: " + questionArray[x].questionCorrectAnswer);
         }
-
-        return questionArray;
-
-    }
-
-    initQuestionArray();
-    
-    initQuestionObject();
-
-    questionMatrix();
 
     initialButtons();
 
