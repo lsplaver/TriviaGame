6afd4321a5d52d53df35e5cba748d63b8278aeb4
unknown <lsplaver@yahoo.com>
2018-03-03 04:51:25 -0500

    Update - inifinite looping

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,6 @@ $(document).ready(function() {
     }
 
     function initializeVariables() {
-        // correctAnswerChosen = false;
-        // totalCorrect = 0;
-        // totalIncorrect = 0;
-        // totalUnanswered = 0;
-        // newGame = true;
         console.log("initial correctAnswerChosen value: " + initCorrAnswrChsn());
         console.log("initial totalCorrect value: " + initTotCorct());
         console.log("initial totalIncorrect value : " + initTotIncorct());
@@ -23,31 +18,36 @@ $(document).ready(function() {
     }
 
     function initCorrAnswrChsn() {
-        var correctAnswerChosen = false;
+        /*var*/ correctAnswerChosen = false;
         return correctAnswerChosen;
     }
 
     function initTotCorct() {
-        var totalCorrect = 0;
+        /*var*/ totalCorrect = 0;
         return totalCorrect;
     }
 
     function initTotIncorct() {
-        var totalIncorrect = 0;
+        /*var*/ totalIncorrect = 0;
         return totalIncorrect;
     }
 
     function initTotUnanswr() {
-        var totalUnanswered = 0;
+        /*var*/ totalUnanswered = 0;
         return totalUnanswered;
     }
 
     function initNewGame() {
-        var newGame = true;
+        /*var*/ newGame = true;
         return newGame;
     }
 
-    var timeRemaining;
+    // function initQuestionArray() {
+    //     questionArray = new Array(0);
+    //     return questionArray;
+    // }
+
+    var timeRemaining, questionArray = [], newGame, correctAnswerChosen, totalCorrect, totalIncorrect, totalUnanswered;
 
     function initTimeRemaining() {
         timeRemaining = 30;
@@ -65,7 +65,7 @@ $(document).ready(function() {
             this.questionCorrectAnswer = questionCorrectAnswer;
     }
 
-    var questionArray = [];
+    // var questionArray = [];
     console.log("The value of questionArray is: " + questionArray);
 
     function setQuestionObject() {
@@ -74,22 +74,22 @@ $(document).ready(function() {
         // console.log("The value of questionObject: " + questionObject.toString());
         // questionArray.push(questionObject);
         questionObject = new QuestionObject(1, "What type of game system is Pathfinder RPG?",
-            "Pen & Paper", "PC", "Figurines", "GURPs", 1);
+            "Pen & Paper", "PC", "Figurines", "GURPs", "Pen & Paper");
         questionArray.push(questionObject);
         questionObject = new QuestionObject(2, "What type of game were the old Fallout games?",
-            "Pen & Paper", "XBox", "ARPG", "RPG", 4);
+            "Pen & Paper", "XBox", "ARPG", "RPG", "RPG");
         questionArray.push(questionObject);
         questionObject = new QuestionObject(3, "What type of game have the newer Fallout games been?",
-            "Pen & Paper", "RPG", "ARPG", "Casual", 3);
+            "Pen & Paper", "RPG", "ARPG", "Casual", "ARPG");
         questionArray.push(questionObject);
         questionObject = new QuestionObject(4, "How many Might and Magic (Not Heroes) games have"
-            + " there been?", "Too Many", "10", "15", "8", 2);
+            + " there been?", "Too Many", "10", "15", "8", "10");
         questionArray.push(questionObject);
         questionObject = new QuestionObject(5, "What type of game is an ARPG?",
-            "Strategy", "Role-Playing", "Action Role-Playing", "Adventure Role-Playing", 3);
+            "Strategy", "Role-Playing", "Action Role-Playing", "Adventure Role-Playing", "Action Role-Playing");
         questionArray.push(questionObject);
         questionObject = new QuestionObject(6, "Which game system does the Baldur's Gate series use?",
-            "Pathfinder", "2nd Edition AD&D", "ShadowRun", "GURPs", 2);
+            "Pathfinder", "2nd Edition AD&D", "ShadowRun", "GURPs", "2nd Edition AD&D");
         questionArray.push(questionObject);
         // questionObject = new QuestionObject(7, "What type of game system is Pathfinder RPG?",
         //     "Pen & Paper", "PC", "Figurines", "GURPs", 1);
@@ -128,7 +128,7 @@ $(document).ready(function() {
 
     initializeVariables();
 
-    initTimeRemaining();
+    // initTimeRemaining();
 
     var intervalId, questionNumber;
 
@@ -137,16 +137,18 @@ $(document).ready(function() {
         return questionNumber;
     }
 
-    initQuestionNumber();
+    // initQuestionNumber();
 
     function initialQuestion() {
         // intervalId = setInterval(decrementTimer, 1000);
         resetIntervalId();
+        questionNumber = initQuestionNumber();
     }
 
-    initialQuestion();
+    // initialQuestion();
 
     function resetIntervalId() {
+        clearInterval(intervalId);
         intervalId = setInterval(decrementTimer, 1000);
     }
 
@@ -154,13 +156,15 @@ $(document).ready(function() {
         if (questionNumber < 6) {
             timeRemaining = initTimeRemaining();
             // intervalId = setInterval(decrementTimer, 1000);
+            populateButtons(questionNumber);
             resetIntervalId();
             return timeRemaining;
         }
         else {
             newGame = true;
+            resetGame(newGame);
             // gameNumber++;
-            initialQuestion();
+            // initialQuestion();
         }
     }
 
@@ -168,7 +172,7 @@ $(document).ready(function() {
         $("#timeRemaining").text("Time Remaining: " + timeRemaining);
         console.log("The current timeRemaining is: " + timeRemaining);
         if (questionNumber === 0) {
-            newGame = false;
+            newGame = true;
             return newGame;
         }
         // return newGame;
@@ -189,9 +193,46 @@ $(document).ready(function() {
             questionNumber++;
             currentQuestion(questionNumber);
         }
+        // else {
+        //     questionNumber++;
+        // }
 
         return timeRemaining;
     }    
 
     decrementTimer(timeRemaining);
+
+    // function randomizeQuestions() {
+    //     var randNumber1, randNumber2, randNumber3, randNumber4;
+    //     randNumber1 = Math.floor((Math.random() * 4) + 1);
+    //     randNumber2 = Math.floor((Math.random() * 4) + 1);
+    //     randNumber3 = Math.floor((Math.random() * 4) + 1);
+    //     randNumber4 = Math.floor((Math.random() * 4) + 1);
+    // }
+
+    function populateButtons(questionNumber) {
+        var x = questionNumber;
+        $("#btnAnswer1").text(questionArray[x].questionAnswer1);
+        $("#btnAnswer2").text(questionArray[x].questionAnswer2);
+        $("#btnAnswer3").text(questionArray[x].questionAnswer3);
+        $("#btnAnswer4").text(questionArray[x].questionAnswer4);
+    }
+
+    if (newGame) {
+        resetGame();
+    }
+
+    function resetGame() {
+        initCorrAnswrChsn();
+        initQuestionNumber();
+        initTotCorct();
+        initTotIncorct();
+        initTotUnanswr();
+        initTimeRemaining();
+        initialQuestion();
+        currentQuestion(questionNumber);
+        // populateButtons(questionNumber);
+        newGame = false;
+        return newGame;
+    }
 })
